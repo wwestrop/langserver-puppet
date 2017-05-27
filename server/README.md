1e343bcb1c9f1244ae623fcc4934d235161374e1
Will Westrop <wwestrop@hotmail.com>
2017-05-27 20:11:12 +0100

    Added some (hardcoded, and incomplete) inbuilt definitions. It's for a POC

--- server/README.md
+++ server/README.md
@@ -1,3 +1,13 @@
-#README
+# README
 
-This is a language server sample implementation in node
+This is a language server for the [Puppet language](https://docs.puppet.com/puppet/4.10/lang_visual_index.html). It can be installed in [Visual Studio Code](https://code.visualstudio.com/), or other editors implementing the [Language Server Protocol](http://langserver.org/), such as Atom or Sublime (full list [here](http://langserver.org/#implementations-client)).
+
+# Currently supported
+* Class, parameter, and value completion for built-in Puppet types
+
+# TODO
+* Completion for include-like statements
+* Completion for class pseudo-resource (resource-style class declaration) 
+* Completion for searching a module path for forge modules and custom types
+* If a module path does not exist, support fetching them from an R10k file
+* Ruby types (I will probably never do this)
--- server/src/server.ts
+++ server/src/server.ts
@@ -43,7 +43,9 @@ connection.onInitialize((params): InitializeResult => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent((change) => {
-	validateTextDocument(change.document);
+	// validateTextDocument(change.document);
+	console.log('hello world');
+	// change.document.
 });
 
 // The settings interface describe the server relevant settings part
@@ -65,32 +67,8 @@ connection.onDidChangeConfiguration((change) => {
 	let settings = <Settings>change.settings;
 	maxNumberOfProblems = settings.languageServerExample.maxNumberOfProblems || 100;
 	// Revalidate any open text documents
-	documents.all().forEach(validateTextDocument);
+	// documents.all().forEach(this.document = textDocument.getText(););
 });
-
-function validateTextDocument(textDocument: TextDocument): void {
-	let diagnostics: Diagnostic[] = [];
-	let lines = textDocument.getText().split(/\r?\n/g);
-	let problems = 0;
-	for (var i = 0; i < lines.length && problems < maxNumberOfProblems; i++) {
-		let line = lines[i];
-		let index = line.indexOf('typescript');
-		if (index >= 0) {
-			problems++;
-			diagnostics.push({
-				severity: DiagnosticSeverity.Warning,
-				range: {
-					start: { line: i, character: index},
-					end: { line: i, character: index + 10 }
-				},
-				message: `${line.substr(index, 10)} should be spelled TypeScript`,
-				source: 'ex'
-			});
-		}
-	}
-	// Send the computed diagnostics to VSCode.
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-}
 
 connection.onDidChangeWatchedFiles((change) => {
 	// Monitored files have change in VSCode
@@ -100,6 +78,19 @@ connection.onDidChangeWatchedFiles((change) => {
 
 // This handler provides the initial list of the completion items.
 connection.onCompletion((textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+
+	// This is a naive parser, rather than re-parsing on every change, the way it works is basically this:
+	// From the current text entry point, walk back until we see what looks like:
+	// 		An include statement (autoprovide the list of available modules/classes)
+	// 		A closing brace } or the delcaration of the type - which indicates autocomplete list of types
+	//
+	// For parameter completion:
+	//		A resource declaration. Then we look up in $modulesDir for that type, presumably we must parse the whole lot
+	//		Presumably, some built-in types don't have source available
+	//
+	// Since the document being edited isn't necessarily in a compilable state, we can't just write a regular parser for it
+	// It's not complete. This is an enterprise product. If they can't develop tools for it, why should I
+
 	// The pass parameter contains the position of the text document in 
 	// which code complete got requested. For the example we ignore this
 	// info and always provide the same completion items.
@@ -141,6 +132,243 @@ connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
 	}
 	return item;
 });
+
+
+
+function doLookups(): void {
+
+}
+
+/** Reads backwards from the current insertion point to determine what "scope" the editor is in,
+ *  and hence what information the user will want available for autocompleting. 
+ */
+function getScope(): ICompletionScope {
+	throw "Not implemented";
+}
+
+interface ICompletionScope {
+	getCompletionItems(): CompletionItem[];
+}
+
+/** The top level of a file where we enter our resources, we want autocompletion on the available resources */
+class ResourceScope implements ICompletionScope {
+	// The same autocompleteable members might apply after an "include " statement or similar
+
+	getCompletionItems(): CompletionItem[] {
+		throw "Not implemented yet";
+	}
+}
+
+/** When we're inside a resource declaration, and we want autocompletion on the list of possible parameters */
+class ParameterScope implements ICompletionScope {
+	private _resource: Resource;
+
+	/** The resource whose parameters we want to enumerate */
+	public constructor(resource: Resource) {
+	}
+
+	getCompletionItems(): CompletionItem[] {
+
+		// this._resource.properties.map(r => {
+		// 	label: r.name,
+		// 	kind: r.type == PuppetType.Enum ? CompletionItemKind.Enum : CompletionItemKind.Property,
+		// 	detail: r.possibleValues // for enums only
+		// });
+
+		throw "Not implemented yet";
+	}
+}
+
+/** The scope when we've entered a parameter, followed by a hash rocket => and want assistance on the available values */
+class ParameterValueScope implements ICompletionScope {
+	getCompletionItems(): CompletionItem[] {
+		throw "Not implemented yet";
+	}
+}
+
+interface IProperty {
+	name: string;
+	type: PuppetType;
+	possibleValues: string[] | undefined;
+}
+
+interface IResource {
+	name: string;
+	properties: IProperty[];
+}
+
+class Resource {
+	private _name: string;
+	private _properties: Property[];
+
+	constructor(name: string, properties: Property[]) {
+		this._name = name;
+		this._properties = properties;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+
+	get properties(): Property[] {
+		return this._properties;
+	}
+}
+
+class Property {
+	private _name: string;
+	private _type: PuppetType;
+	private _possibleValues: string[] | undefined;
+
+	// TODO: not supporting union or parameterised types except for enums (it's all quite loosely typed anyway)
+	/** @param possibleValues (For enum types only) - lists the values the property can take */
+	constructor(name: string, type: PuppetType, possibleValues?: string[]) {
+		this._name = name;
+		this._type = type;
+		this._possibleValues = possibleValues;
+	}
+
+	get name(): string {
+		return this._name;
+	}
+
+	get type(): PuppetType {
+		return this._type;
+	}
+
+	get possibleValues(): string[] | undefined {
+		return this._possibleValues;
+	}
+}
+
+enum PuppetType {
+	Variant,
+	Boolean,
+	String,
+	Integer,
+	Float,
+	Numeric,
+	Enum,
+	Resource,
+	Class,
+	Array,
+	Hash,
+	Regexp,
+	Undef,
+	Scalar,
+	Collection,
+	Data,
+	Pattern,
+	Tuple,
+	Struct,
+	Optional,
+	CatalogEntry,
+	Type,
+	Any,
+	Callable
+}
+
+
+function parseModules(): IResource[] {
+	// look at the modile path. /etc/puppetlabs/code/environments/$ENV/modules/**/*.pp or whatever
+	// plus the built-in types
+
+	let builtInResources: IResource[] = [
+		{
+			name: 'file',
+			properties: [
+			{
+				name: 'ensure',
+				type: PuppetType.Enum,
+				possibleValues: ['file', 'directory', 'present', 'absent', 'link']
+			},
+			{
+				name: 'content',
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'mode',
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'group',
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'owner',
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'source',	// TODO: add extra docs for this paramter (file:// http:// puppet:// etc etc)
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'target',
+				type: PuppetType.String,
+				possibleValues: undefined
+			}]
+		},
+		{
+			name: 'exec',
+			properties: [
+			{
+				name: 'provider',
+				type: PuppetType.Enum,
+				possibleValues: ['powershell', 'posix', 'shell', 'windows']
+			},
+			{
+				name: 'content',
+				type: PuppetType.String,
+				possibleValues: undefined
+			},
+			{
+				name: 'mode',
+				type: PuppetType.String,
+				possibleValues: undefined
+			}]
+		},
+		{
+			name: 'package',
+			properties: [
+			{
+				name: 'provider',
+				type: PuppetType.Enum,
+				possibleValues: ['apple', 'apt', 'dpkg', 'gem', 'rpm', 'windows', 'yum']
+			},
+			{
+				name: 'ensure',
+				type: PuppetType.Enum,
+				possibleValues: ['present', 'installed', 'absent', 'purged', 'held', 'latest']
+			},
+			{
+				name: 'install',
+				type: PuppetType.Array,
+				possibleValues: undefined
+			},
+			{
+				name: 'reinstall_on_refresh',		// TODO extra docs about, for example default values
+				type: PuppetType.Boolean,
+				possibleValues: undefined
+			},
+			{
+				name: 'source',
+				type: PuppetType.String,
+				possibleValues: undefined
+			}]
+		}
+	];
+
+	//throw "";
+	return builtInResources;
+}
+
+
+
 
 let t: Thenable<string>;
 
